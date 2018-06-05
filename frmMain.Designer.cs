def4758b8c0a2f46f4e95efe646eed5f575901f8
Tomozaki <zhangxinran@tomozaki.com>
2018-06-05 17:01:31 +0800

    Init Project

---
+++ frmMain.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace GoogleTranslateTool
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.comboFrom = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboTo = new System.Windows.Forms.ComboBox();
+            this.btnGoogleTranslate = new System.Windows.Forms.Button();
+            this.btnPdfFormat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Location = new System.Drawing.Point(12, 57);
+            this.txtFrom.Multiline = true;
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtFrom.Size = new System.Drawing.Size(400, 477);
+            this.txtFrom.TabIndex = 0;
+            this.txtFrom.Text = resources.GetString("txtFrom.Text");
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(420, 57);
+            this.txtTo.Multiline = true;
+            this.txtTo.Name = "txtTo";
+            this.txtTo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtTo.Size = new System.Drawing.Size(400, 477);
+            this.txtTo.TabIndex = 1;
+            // 
+            // comboFrom
+            // 
+            this.comboFrom.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboFrom.FormattingEnabled = true;
+            this.comboFrom.Location = new System.Drawing.Point(12, 22);
+            this.comboFrom.Name = "comboFrom";
+            this.comboFrom.Size = new System.Drawing.Size(121, 20);
+            this.comboFrom.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 11F);
+            this.label1.Location = new System.Drawing.Point(139, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "->";
+            // 
+            // comboTo
+            // 
+            this.comboTo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboTo.FormattingEnabled = true;
+            this.comboTo.Location = new System.Drawing.Point(169, 21);
+            this.comboTo.Name = "comboTo";
+            this.comboTo.Size = new System.Drawing.Size(121, 20);
+            this.comboTo.TabIndex = 4;
+            // 
+            // btnGoogleTranslate
+            // 
+            this.btnGoogleTranslate.Location = new System.Drawing.Point(296, 20);
+            this.btnGoogleTranslate.Name = "btnGoogleTranslate";
+            this.btnGoogleTranslate.Size = new System.Drawing.Size(75, 23);
+            this.btnGoogleTranslate.TabIndex = 5;
+            this.btnGoogleTranslate.Text = "翻译";
+            this.btnGoogleTranslate.UseVisualStyleBackColor = true;
+            this.btnGoogleTranslate.Click += new System.EventHandler(this.btnGoogleTranslate_Click);
+            // 
+            // btnPdfFormat
+            // 
+            this.btnPdfFormat.Location = new System.Drawing.Point(377, 20);
+            this.btnPdfFormat.Name = "btnPdfFormat";
+            this.btnPdfFormat.Size = new System.Drawing.Size(109, 23);
+            this.btnPdfFormat.TabIndex = 6;
+            this.btnPdfFormat.Text = "PDF格式优化";
+            this.btnPdfFormat.UseVisualStyleBackColor = true;
+            this.btnPdfFormat.Click += new System.EventHandler(this.btnPdfFormat_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 546);
+            this.Controls.Add(this.btnPdfFormat);
+            this.Controls.Add(this.btnGoogleTranslate);
+            this.Controls.Add(this.comboTo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboFrom);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.txtFrom);
+            this.Name = "frmMain";
+            this.Text = "谷歌翻译工具";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.ComboBox comboFrom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboTo;
+        private System.Windows.Forms.Button btnGoogleTranslate;
+        private System.Windows.Forms.Button btnPdfFormat;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,192 @@
+﻿using Microsoft.JScript.Vsa;
+using System;
+using System.CodeDom.Compiler;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Windows.Forms;
+
+namespace GoogleTranslateTool
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private string[] arrLanguage = { "zh-CN", "en", "ja", "de" };
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            comboFrom.Items.Clear();
+            comboFrom.Items.Add("auto");
+            comboFrom.Items.AddRange(arrLanguage);
+            comboFrom.SelectedIndex = 0;
+
+            comboTo.Items.Clear();
+            comboTo.Items.AddRange(arrLanguage);
+            comboTo.SelectedIndex = 0;
+        }
+
+        private void btnPdfFormat_Click(object sender, EventArgs e)
+        {
+            string[] arrData = Regex.Split(txtFrom.Text, "\\r\\n");
+            Console.WriteLine(arrData.Length);
+            StringBuilder strData = new StringBuilder();
+            strData.Clear();
+            foreach (string item in arrData)
+            {
+                if (item.Length > 0)
+                {
+                    byte firstChar = System.Text.Encoding.Default.GetBytes(item)[0];
+                    if ((firstChar >= 'a' && firstChar <= 'z') || firstChar == ' ')
+                    {
+                        strData.Append(" " + item);
+                    }
+                    else
+                    {
+                        strData.Append("\r\n" + item);
+                    }
+                }
+            }
+            txtFrom.Text = strData.ToString().Trim();
+        }
+
+        private void btnGoogleTranslate_Click(object sender, EventArgs e)
+        {
+            string strFrom = comboFrom.SelectedItem.ToString();
+            string strTo = comboTo.SelectedItem.ToString();
+            string[] arrData = Regex.Split(txtFrom.Text, "\\r\\n");
+            StringBuilder strData = new StringBuilder();
+            foreach (string item in arrData)
+            {
+                strData.Append("\r\n");
+                if (item.Trim().Length > 0)
+                {
+                    strData.Append(GoogleTranslate(item, strFrom, strTo));
+                }
+            }
+            txtTo.Text = strData.ToString().Trim();
+        }
+
+        #region 谷歌翻译
+        /// <summary>
+        /// 谷歌翻译
+        /// </summary>
+        /// <param name="text">待翻译文本</param>
+        /// <param name="fromLanguage">自动检测：auto</param>
+        /// <param name="toLanguage">中文：zh-CN，英文：en</param>
+        /// <returns>翻译后文本</returns>
+        public string GoogleTranslate(string text, string fromLanguage, string toLanguage)
+        {
+            CookieContainer cc = new CookieContainer();
+
+            string GoogleTransBaseUrl = "https://translate.google.cn/";
+
+            string BaseResultHtml = GetResultHtml(GoogleTransBaseUrl, cc, "");
+
+            Regex re = new Regex(@"(?<=TKK=)(.*?)(?=\);)");
+
+            string TKKStr = re.Match(BaseResultHtml).ToString() + ")";//在返回的HTML中正则匹配TKK的JS代码
+
+            string TKK = EvalJScript(TKKStr).ToString();
+            Console.WriteLine("TKK:" + TKK);
+
+            string GetTkkJS = File.ReadAllText("./gettk.js");
+
+            string tk = GetJsMethd(GetTkkJS, "tk", new object[] { text, TKK});
+            Console.WriteLine("tk:" + tk);
+
+            string googleTransUrl = "https://translate.google.cn/translate_a/single?client=t&sl=" + fromLanguage + "&tl=" + toLanguage + "&hl=en&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&ie=UTF-8&oe=UTF-8&otf=1&ssel=0&tsel=0&kc=1&tk=" + tk + "&q=" + HttpUtility.UrlEncode(text);
+
+            string ResultHtml = GetResultHtml(googleTransUrl, cc, "https://translate.google.cn/");
+
+            dynamic TempResult = Newtonsoft.Json.JsonConvert.DeserializeObject(ResultHtml);
+
+            string ResultText = Convert.ToString(TempResult[0][0][0]);
+
+            return ResultText;
+        }
+
+        public string GetResultHtml(string url, CookieContainer cookie, string referer, string useragent = "")
+        {
+            var html = "";
+
+            var webRequest = WebRequest.Create(url) as HttpWebRequest;
+
+            webRequest.Method = "GET";
+
+            webRequest.CookieContainer = cookie;
+
+            webRequest.Referer = referer;
+
+            webRequest.Timeout = 20000;
+
+            webRequest.Headers.Add("X-Requested-With:XMLHttpRequest");
+
+            webRequest.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+
+            //webRequest.UserAgent = useragent;
+
+            using (var webResponse = (HttpWebResponse)webRequest.GetResponse())
+            {
+                using (var reader = new StreamReader(webResponse.GetResponseStream(), Encoding.UTF8))
+                {
+
+                    html = reader.ReadToEnd();
+                    reader.Close();
+                    webResponse.Close();
+                }
+            }
+            return html;
+        }
+
+        /// <summary>
+        /// 执行JS方法
+        /// </summary>
+        /// <param name="methodName">方法名</param>
+        /// <param name="para">参数</param>
+        /// <returns></returns>
+        private static string GetJsMethd(string sCode, string methodName, object[] para)
+        {
+
+            CompilerParameters parameters = new CompilerParameters();
+
+            parameters.GenerateInMemory = true;
+
+            CodeDomProvider _provider = new Microsoft.JScript.JScriptCodeProvider();
+
+            CompilerResults results = _provider.CompileAssemblyFromSource(parameters, sCode);
+
+            Assembly assembly = results.CompiledAssembly;
+
+            Type _evaluateType = assembly.GetType("aa.JScript");
+
+            object obj = _evaluateType.InvokeMember(methodName, BindingFlags.InvokeMethod, null, null, para);
+
+            return obj.ToString();
+        }
+
+        public static object EvalJScript(string JScript)
+        {
+            object Result = null;
+            try
+            {
+                Result = Microsoft.JScript.Eval.JScriptEvaluate(JScript, VsaEngine.CreateEngine());
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            return Result;
+
+        }
+        #endregion
+
+
+
+    }
+}
